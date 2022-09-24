2aa8671a93e451c6c511947d1287f28689645e93
NoodlesCoder18 <mounibe2308@gmail.com>
2022-09-24 15:36:34 +0530

    Initial commit

---
+++ src/containers/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+// import {NavLink } from 'react-router-dom';
+
+
+const Header = () => {
+  return (
+    <div className="ui fixed menu">
+      <div className="ui container center">
+        <h2>FakeShop</h2>
+        {/* <NavLink to="/"></NavLink> */}
+      
+      </div>
+    </div>
+  );
+};
+
+export default Header;
